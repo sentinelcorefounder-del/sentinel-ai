c33b3020bfaeda785af8099b258e874514207497
sentinelcorefounder-del <sentinel.core.founder@gmail.com>
2026-03-23 18:31:21 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 device = torch.device("cpu")
 
