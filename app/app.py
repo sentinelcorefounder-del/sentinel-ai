7cd7d5d99339982ae596663f5f45c52a5bed192c
sentinelcorefounder-del <sentinel.core.founder@gmail.com>
2026-03-24 04:24:08 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -241,6 +241,5 @@ def index():
 
 # =========================
 if __name__ == "__main__":
-    if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
